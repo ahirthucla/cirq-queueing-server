1eb31415847f202f7e1a825c10f43790db717f86
ahirthucla <ahirth@ucla.edu>
2021-02-20 04:51:45 +0000

    renaming and first step of turning into a proper python module

---
+++ README.md
@@ -0,0 +1,1 @@
+A small server, job-verifier, and job-processor for quantum programs on Google's Sycamore device, using google cloud
--- job_processor/job_processor.py
+++ job_processor/job_processor.py
@@ -1,14 +1,10 @@
-from typing import TYPE_CHECKING
 from google.cloud import datastore 
 import cirq
-from cirq.contrib.qasm_import import circuit_from_qasm, QasmException
 from flask import Flask
 import time
-import cirq.google as cg
 import sys
-
-if TYPE_CHECKING: 
-    from google.cloud.datastore import Entity
+from cirq.contrib.qasm_import import circuit_from_qasm, QasmException
+from quantum_circuit_multiplexers.multiplex import multiplex_onto_sycamore, get_error_qubits
 
 # Connect to datastore
 client = datastore.Client()
@@ -17,33 +13,20 @@ client = datastore.Client()
 app = Flask(__name__)
 
 # processor id from argument
-processor_id = str(sys.argv[1])
+PROCESSOR_ID = str(sys.argv[1])
 
-# Initialize google cloud quantum engine
-engine = cirq.google.Engine(project_id=client.project)
-
-# Initialize handlers for qsim and real hardware
-qsim_handler = engine.sampler(processor_id='qsim', gate_set=cg.SYC_GATESET)
-real_handler = engine.sampler(processor_id=processor_id, gate_set=cg.SYC_GATESET)
-
-# Map 'method' strings to handlers
-handlers = {'csim':cirq.Simulator(), 'qsim':qsim_handler, 'real': real_handler}
-
-#TODO multiplexing job circuits, demultiplexing the results, storing in each job
-
-#def run_job(entity: 'Entity', handlers: whatever) -> 'Entity':
-def run_job(entity: 'Entity') -> 'Entity':
+def run_job(entity: 'datastore.Entity', engine) -> 'datastore.Entity':
     """ Run job on one of available handlers and update entity
     Arg: 
-        entity: unfinished job
+        entity: unfinished job key-able entity
     Returns: 
         entity with updated results or response message
     """
 
     # ensure only running unfinished jobs
-    assert not entity['done']:
+    assert not entity['done']
 
-    # mark entity as done
+    # mark entity as done TODO
     entity['done'] = True
 
     # parse circuit
@@ -52,24 +35,30 @@ def run_job(entity: 'Entity') -> 'Entity':
     try:
         circuit = circuit_from_qasm(entity['qasm'])
     except QasmException as e:
-        entity['message'] = 'Error converting QASM string to circuit:\n' +\
-                            'Exception Observed:\n' + str(e) + '\n' + \
+        entity['message'] = 'Exception observed while converting QASM string to circuit:\n' + str(e) + '\n' + \
                             'With QASM string:\n' + str(entity['qasm'])
         return entity
     
-    # select handler based on 'method' type
-    if 'method' in entity and entity['method'] in handlers:
-        handler = handlers[entity['method']]
-    else:
-        handler = handlers['csim']
+    # get handler and device
+    handler = engine.sampler(processor_id=PROCESSOR_ID, gate_set=cirq.google.SYC_GATESET)
+    device = engine.get_processor(processor_id=PROCESSOR_ID).get_device([cirq.google.SYC_GATESET])
+
+    # get current error qubits from recent calibration
+    err_qubits = get_error_qubits(client.project, PROCESSOR_ID, 25)
+
+    # conditionally map circuit
+    try:
+        circuit, _ = next(multiplex_onto_sycamore([circuit], device, exclude_always=err_qubits))
+    except Exception as e:
+        entity['message'] = 'Exception observed while mapping circuit:\n' + str(e)
+        return entity
 
     # time and run circuit
     start = time.time()
     try:
-        result = handler.run(circuit, repetitions=int(entity['shots']))
+        result = handler.run(circuit, repetitions=int(entity['repetitions']))
     except Exception as e:
-        entity['message'] = "Error running circuit:\n"+\
-                 "Exception Observed:\n" + str()
+        entity['message'] = "Exception observed while  running circuit:\n"+ str(e)
         return entity
     elapsed = time.time() - start
 
@@ -86,7 +75,8 @@ def run() -> str:
         string message with number of jobs run
     """
 
-    # tentatively create handlers in here if this isn't going to be a server.
+    # Initialize google cloud quantum engine
+    engine = cirq.google.Engine(project_id=client.project)
 
     # pull unfinished, verified job keys
     query = client.query(kind="job")
@@ -99,8 +89,7 @@ def run() -> str:
     for key in keys:
         with client.transaction():
             entity = client.get(key.key)
-            #entity = run_job(entity, handlers)
-            entity = run_job(entity)
+            entity = run_job(entity, engine)
             client.put(entity)
 
     # return number of jobs run
@@ -108,6 +97,7 @@ def run() -> str:
 
 if __name__ == '__main__':
     # tentatively initialize client here
+    print(run())
 
     # run as local server for testing
-    app.run(host='127.0.0.1', port=8080, debug=True)
+    #app.run(host='127.0.0.1', port=8080, debug=True)
